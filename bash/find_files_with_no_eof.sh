083d124a65b6d77e1b8f826d746b85fcaed6be80
Zafar Khaydarov <zafar@linux.com>
2018-05-30 21:27:16 -0400

    Update find_files_with_no_eof.sh

--- bash/find_files_with_no_eof.sh
+++ bash/find_files_with_no_eof.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#
+# List files which do not have EOL feed
+#
+
 for i in $(find . -iname *.scala); do
 	if diff /dev/null "$i" | tail -1 | grep '^\\ No newline' > /dev/null; then  echo $i;
 	fi;
